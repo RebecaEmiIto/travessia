a25378fc64dbcec16b948fc9637756979b942439
Rebeca Ito <emi.ito@edu.unifil.br>
2021-04-24 15:08:03 -0300

    commit 6

--- esqueleto-jogo-agentes-ia-master/regras_jogo/regras_travessia.py
+++ esqueleto-jogo-agentes-ia-master/regras_jogo/regras_travessia.py
@@ -21,8 +21,7 @@ class RegrasTravessia(AbstractRegrasJogo):
         return self.id_personagem[personagem]
 
     def isFim(self) -> bool:
-        # Se Lado direito = 0, encerra jogo
-        return super().isFim()
+        return self.t1["Direita"] == len(8)
 
     def gerarCampoVisao(self, id_agente):
         """ Retorna um EstadoJogoView para ser consumido por um agente
@@ -32,16 +31,22 @@ class RegrasTravessia(AbstractRegrasJogo):
         EstadoJogoView é um objeto imutável ou uma cópia do jogo, de forma que
         sua manipulação direta não tem nenhum efeito no mundo de jogo real.
         """
-        return
+        percepcoes_jogador = PercepcoesJogador(
+            personagens = set(self.t1),
+            mensagem_jogo = self.msg_jogador)
+
+        self.msg_jogador = None
+        return percepcoes_jogador
+        
 
     def registrarProximaAcao(self, id_agente, acao):
         """ Informa ao jogo qual a ação de um jogador especificamente.
         Neste momento, o jogo ainda não é transformado em seu próximo estado,
- s       isso é feito no método de atualização do mundo.
+        isso é feito no método de atualização do mundo.
         """
-        return
-
+        self.acoes_personagens[id_agente] = acaon
     def atualizarEstado(self, diferencial_tempo):
+        cont = 0
         """ Apenas neste momento o jogo é atualizado para seu próximo estado
         de acordo com as ações de cada jogador registradas anteriormente.
         """
@@ -49,8 +54,32 @@ class RegrasTravessia(AbstractRegrasJogo):
             self.id_personagem[Personagens.O_JOGADOR]]
         if acao_jogador.tipo == AcoesJogador.Selecionar_Indivíduo:
             pass
+
+            p1, p2 = acao_jogador.parametros
+
+            if (p1, p2) in self.t1:
+
+                peca_adjacente = (x + x_mov, y + y_mov)
+                espaco_pos_adjacente = (x + x_mov*2, y + y_mov*2)
+                if self.ValidacaoDireitaEsquerda() is True:
+                    if cont % 2 == 0:
+                        self.t1.discard(peca_adjacente)
+                        self.t1.discard((x,y))
+                        self.t1.add(espaco_pos_adjacente)
+                        cont += 1
+                    else:
+                        self.t1.discard(peca_adjacente)
+                        self.t1.discard((x,y))
+                        self.t1.add(espaco_pos_adjacente)
+                        cont += 1
+                else:
+                    self.msg_jogador = f'Direção especificada para movimento não é possível.'
+            else:
+                self.msg_jogador = f'Não há bolinha na coordenada especificada.'
+        else:
+            self.msg_jogador = f'Ação especificada inválida.'    
             
-        return
+        return 
 
     def terminarJogo(self):
         """ Faz procedimentos de fim de jogo, como mostrar placar final,
@@ -71,10 +100,12 @@ class RegrasTravessia(AbstractRegrasJogo):
                         if "Filha1" or "Filha2" in esquerda:
                             if "Mae" in esquerda: return True
                             else: return False
+
                     if esquerda[j] == "Mae":
                         if "Filho1" or "Filho2" in esquerda:
                             if "Pai" in esquerda: return True
                             else: return False
+                    
                     if esquerda[j] == "Prisioneira":
                         if "Policial" in esquerda: return True
                         elif len(esquerda) == 1: return True
@@ -88,10 +119,12 @@ class RegrasTravessia(AbstractRegrasJogo):
                         if "Filha1" or "Filha2" in esquerda:
                             if "Mae" in esquerda: return True
                             else: return False
+                    
                     if esquerda[j] == "Mae":
                         if "Filho1" or "Filho2" in esquerda:
                             if "Pai" in esquerda: return True
                             else: return False
+                    
                     if esquerda[j] == "Prisioneira":
                         if "Policial" in esquerda: return True
                         elif len(esquerda) == 1: return True
